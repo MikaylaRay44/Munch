8fcbc3100d154cd9ae0e442febeae8f880812a9b
MikaylaRay <mikaylaray44@gmail.com>
2019-02-16 22:41:41 -0500

    Added framework for logging, currently only logging to a file but has room for adding multiple log targets.

---
+++ Main.vb
@@ -0,0 +1,13 @@
+﻿Imports NLog
+Imports NLog.LogManager
+
+Public Class Main
+
+    Public Shared logger As Logger
+
+    Public Sub New()
+        InitializeComponent()
+        logger = GetCurrentClassLogger()
+    End Sub
+
+End Class
--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Munch.Form1
+            Me.MainForm = Global.Munch.Main
         End Sub
     End Class
 End Namespace
